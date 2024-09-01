1a53e9310b91cf166e93cc88ce8144db8a1102cc
Valyn Tyler <git@valyntyler.com>
2024-09-01 17:05:03 +0300

    ws echo server

--- ws-server/main.py
+++ ws-server/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import asyncio
+from websockets.asyncio.server import serve
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.get_running_loop().create_future()  # run forever
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+asyncio.run(main())
