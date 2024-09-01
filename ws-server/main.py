2e1ffd3f32631029aceaa256a2ac7c9ae6a0cea5
Valyn Tyler <git@valyntyler.com>
2024-09-01 19:56:22 +0300

    update main.py

--- ws-server/main.py
+++ ws-server/main.py
@@ -3,10 +3,11 @@ from websockets.asyncio.server import serve
 
 async def handler(websocket):
     async for message in websocket:
-        print(message)
+        print("Message received: " + message)
 
 async def main():
     async with serve(handler, "localhost", 8765):
         await asyncio.get_running_loop().create_future()  # run forever
 
+print("Starting WS server...")
 asyncio.run(main())
