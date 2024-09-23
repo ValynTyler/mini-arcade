1111f37fcdce53a97534507111ad77e41909ff3c
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-09-23 13:55:02 +0300

    added print method for controller

--- controller/__init__.py
+++ controller/__init__.py
@@ -15,4 +15,18 @@ class Joystick:
 class Controller:
     def __init__(self):
         self.dpad: DPad = DPad()
-        self.joystick: Joystick = Joystick()
+        self.joystick = Joystick()
+
+    def print(self):
+        print(
+            "dpad(" +
+            "up",
+            int(self.dpad.up),
+            "down",
+            int(self.dpad.down),
+            "left",
+            int(self.dpad.left),
+            "right",
+            str(int(self.dpad.right)) +
+            ")",
+        )
