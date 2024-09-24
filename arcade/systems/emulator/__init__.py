4b6f2658def17377981820c2874350c080fa8c8a
Valyn Tyler <git@valyntyler.com>
2024-09-24 20:08:42 +0300

    added emulator arrow key support

--- arcade/systems/emulator/__init__.py
+++ arcade/systems/emulator/__init__.py
@@ -13,18 +13,14 @@ if __name__ == "__main__":
     pygame.display.set_caption("controller emulator")
 
 
-def run(controller: Controller):
-    running: bool = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-        keys = pygame.key.get_pressed()
-
+def update(controller: Controller, use_arrow_keys: bool = False):
+    keys = pygame.key.get_pressed()
+    if use_arrow_keys:
+        controller.dpad.up = keys[pygame.K_UP]
+        controller.dpad.down = keys[pygame.K_DOWN]
+        controller.dpad.left = keys[pygame.K_LEFT]
+        controller.dpad.right = keys[pygame.K_RIGHT]
+    else:
         controller.dpad.up = keys[pygame.K_w]
         controller.dpad.down = keys[pygame.K_s]
         controller.dpad.left = keys[pygame.K_a]
@@ -32,4 +28,13 @@ def run(controller: Controller):
 
 
 if __name__ == "__main__":
-    run(Controller())
+    running = True
+    while running:
+        update(Controller())
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
