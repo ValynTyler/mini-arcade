4ce0936dae35069f860249b5046e8b060e00e73c
Valyn Tyler <git@valyntyler.com>
2024-09-27 15:44:55 +0300

    restructured project

---
+++ emulator/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+from common.context import Context
+
+
+def update():
+    keys = pygame.key.get_pressed()
+
+
+if __name__ == "__main__":
+    ctx = Context(caption="emulator")
+
+    print("Starting emulator...")
