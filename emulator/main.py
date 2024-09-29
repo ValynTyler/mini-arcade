bdc213a07e4aa7f15c6108f17e39850082acf16e
Valyn Tyler <git@valyntyler.com>
2024-09-30 01:07:37 +0300

    emulator now sending data over ws

--- emulator/main.py
+++ emulator/main.py
@@ -1,35 +1,24 @@
-import asyncio
-import threading
-
 import pygame
-import websockets
-from websockets.asyncio.client import connect
+from websockets.sync.client import connect
 
 from common.context import Context
 from common.controller import Controller
 
 
-async def websocket_client(uri):
-    async with websockets.connect(uri) as websocket:
-        # Send a message to the server
-        await websocket.send("Hello, Server!")
-        print(f"> Sent: Hello, Server!")
-
-        # Wait for a response from the server
-        response = await websocket.recv()
-        print(f"< Received: {response}")
-
-
 class Emulator:
-    websocket_url = "localhost:8765"
     websocket = None
 
     def __init__(self):
         self.controller = Controller()
 
     def start(self):
-        loop = ctx.run(self.update)
-        loop()
+        with connect("ws://localhost:8765") as websocket:
+            websocket.send("Sending message from emulator")
+            websocket.recv()
+            self.websocket = websocket
+
+            loop = ctx.run(self.update)
+            loop()
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -38,22 +27,12 @@ class Emulator:
         self.controller.dpad.left = keys[pygame.K_a]
         self.controller.dpad.right = keys[pygame.K_d]
 
-        print(self.controller)
-
-    async def connect_websocket(self):
-        with connect(self.websocket_url) as websocket:
-            self.websocket = websocket
-            print("Websocket connected successfully")
-
-    def websocket_client(self):
-        asyncio.run(self.connect_websocket())
+        self.websocket.send(self.controller.__str__())
+        self.websocket.recv()
 
 
 if __name__ == "__main__":
     print("Starting emulator...")
     ctx = Context(width=200, height=200, caption="emulator")
     emu = Emulator()
-
-    ws_thread = threading.Thread(target=emu.websocket_client())
-
     emu.start()
