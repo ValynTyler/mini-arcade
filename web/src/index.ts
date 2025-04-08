74d9a3d551a501bb94ed16fdb9253690ae2224a4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-08 22:22:24 +0300

    feat: init phaser

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,44 @@
-const count_btn = document.querySelector("button")
+import Phaser from 'phaser'
 
-let count = 0
+class Example extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL('https://labs.phaser.io');
 
-count_btn?.addEventListener('click', () => {
-  count += 1
-  count_btn.textContent = count.toString()
-})
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    this.load.image('red', 'assets/particles/red.png');
+  }
+
+  create() {
+    this.add.image(400, 300, 'sky');
+
+    const particles = this.add.particles(0, 0, 'red', {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    });
+
+    const logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    particles.startFollow(logo);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: Example,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 200 }
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
