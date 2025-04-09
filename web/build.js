78bf6f439a8c5dcecbe3f9823c301a3a6873121c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-09 17:35:17 +0300

    fix: build.js path resolution

--- web/build.js
+++ web/build.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
+import url from 'url'
+import path from 'path'
+
 import * as esbuild from 'esbuild'
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 let ctx = await esbuild.context({
-  entryPoints: ['src/index.ts'],
+  entryPoints: [ path.join(__dirname, 'src/index.ts') ],
   bundle: true,
-  outdir: 'public',
+  outdir: path.join(__dirname, 'public'),
 })
 
 await ctx.watch()
