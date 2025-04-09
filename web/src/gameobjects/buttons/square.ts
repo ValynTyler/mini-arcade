c60e3077572cd17c3f54751fd346b1b7c1a23868
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-10 00:49:25 +0300

    refactor: create buttons/square.ts

---
+++ web/src/gameobjects/buttons/square.ts
@@ -0,0 +1,76 @@
+export default class SquareButton extends Phaser.GameObjects.Container {
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    super(scene);
+
+    this.scene = scene;
+    this.x = x;
+    this.y = y;
+
+    this.base = this.scene.add
+      .rectangle(0, 0, this.base_size, this.base_size, this.base_color)
+
+    this.face = this.scene.add
+      .rectangle(0, 0, undefined, undefined, this.face_color)
+      .setInteractive()
+      .on("pointerdown", this.onButtonPress)
+      .on("pointerup", this.onButtonRelease)
+      .on("pointerout", this.onButtonRelease)
+
+    this.stem = this.scene.add
+      .rectangle(0, 0, 0, 0, this.stem_color)
+      .setOrigin(0.5, 1)
+      .setInteractive()
+      .on("pointerdown", this.onButtonPress)
+      .on("pointerup", this.onButtonRelease)
+      .on("pointerout", this.onButtonRelease)
+
+    this.add(this.base)
+    this.add(this.stem)
+    this.add(this.face)
+
+    this.scene.add.existing(this)
+  }
+
+  btn_size: number = 50
+  base_size: number = 70
+  height: number = 15
+
+  face_color: number = 0xff0000
+  stem_color: number = 0xbb0000
+  base_color: number = 0x000000
+
+  private target_height: number = this.height
+  private actual_height: number = this.height
+
+  private face!: Phaser.GameObjects.Rectangle
+  private stem!: Phaser.GameObjects.Rectangle
+  private base!: Phaser.GameObjects.Rectangle
+
+  private onButtonRelease = () => {
+    this.target_height = this.height
+    this.redrawUI()
+  }
+
+  private onButtonPress = () => {
+    this.target_height = 0
+    this.redrawUI()
+  }
+
+  private redrawUI = () => {
+    const stem_position = { x: 0, y: this.btn_size / 2 }
+    const face_position = { x: 0, y: -this.actual_height }
+
+    const stem_size = { width: this.btn_size, height: this.actual_height }
+    const face_size = { width: this.btn_size, height: this.btn_size }
+
+    this.stem.setPosition(stem_position.x, stem_position.y).setSize(stem_size.width, stem_size.height)
+    this.face.setPosition(face_position.x, face_position.y).setSize(face_size.width, face_size.height)
+  }
+
+  preUpdate() { this.update() }
+
+  update() {
+    this.actual_height = Phaser.Math.Linear(this.target_height, this.actual_height, 0.5)
+    this.redrawUI()
+  }
+}
--- web/src/scenes/example.ts
+++ web/src/scenes/example.ts
@@ -1,3 +1,5 @@
+import SquareButton from "../gameobjects/buttons/square"
+
 export default class Example extends Phaser.Scene {
   constructor() {
     super({
@@ -5,92 +7,19 @@ export default class Example extends Phaser.Scene {
     })
   }
 
-  btn_size: number = 50
-  base_size: number = 70
-  height: number = 15
-
-  face_color: number = 0xff0000
-  stem_color: number = 0xbb0000
-  base_color: number = 0x000000
-
-  private target_height: number = this.height
-  private actual_height: number = this.height
-
-  private target_face_color: number = this.face_color
-  private target_stem_color: number = this.stem_color
-  private actual_face_color: number = this.face_color
-  private actual_stem_color: number = this.stem_color
-
-  private face!: Phaser.GameObjects.Rectangle
-  private stem!: Phaser.GameObjects.Rectangle
-  private base!: Phaser.GameObjects.Rectangle
-
-  private onButtonRelease = () => {
-    this.target_height = this.height
-    this.target_face_color = this.face_color
-    this.redrawUI()
-  }
-
-  private onButtonPress = () => {
-    this.target_height = 0
-    this.target_face_color = this.stem_color
-    this.redrawUI()
-  }
-
-  private redrawUI = () => {
-    const stem_position = { x: 0, y: this.btn_size / 2 }
-    const face_position = { x: 0, y: -this.actual_height }
-
-    const stem_size = { width: this.btn_size, height: this.actual_height }
-    const face_size = { width: this.btn_size, height: this.btn_size }
-
-    this.stem.setPosition(stem_position.x, stem_position.y).setSize(stem_size.width, stem_size.height).setFillStyle(this.actual_stem_color)
-    this.face.setPosition(face_position.x, face_position.y).setSize(face_size.width, face_size.height).setFillStyle(this.actual_face_color)
-  }
-
   create() {
     const vw = this.game.config.width as number / 100
     const vh = this.game.config.height as number / 100
 
-    const asdf = 0
-    const qwer = 0
-
-    this.base = this
-      .add
-      .rectangle(0, 0, this.base_size, this.base_size, this.base_color)
-      // .setStrokeStyle(25, this.base_color)
-
-    this.face = this
-      .add
-      .rectangle(0, 0, undefined, undefined, this.face_color)
-      // .setStrokeStyle(1, 0x00ff00)
-      .setInteractive()
-      .on("pointerdown", this.onButtonPress)
-      .on("pointerup", this.onButtonRelease)
-      .on("pointerout", this.onButtonRelease)
-
-    this.stem = this
-      .add
-      .rectangle(0, 0, 0, 0, this.stem_color)
-      // .setStrokeStyle(1, 0x0000ff)
-      .setOrigin(0.5, 1)
-      .setInteractive()
-      .on("pointerdown", this.onButtonPress)
-      .on("pointerup", this.onButtonRelease)
-      .on("pointerout", this.onButtonRelease)
-
-    this.add.container(50 * vw, 50 * vh, [
-      this.base,
-      this.face,
-      this.stem,
-    ])
-
-    this.redrawUI()
-  }
-
-  update () {
-    this.actual_height = Phaser.Math.Linear(this.target_height, this.actual_height, 0.5)
-
-    this.redrawUI()
+    const square_button = (this.add as any).squareButton(50 * vw, 50 * vh)
   }
 }
+
+Phaser.GameObjects.GameObjectFactory.register('squareButton', function (this: Phaser.GameObjects.GameObjectFactory, x: number, y: number) {
+  const button = new SquareButton(this.scene, x, y)
+
+  this.displayList.add(button)
+  this.updateList.add(button)
+
+  return button
+})
