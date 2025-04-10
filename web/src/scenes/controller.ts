70d82f1861c22a7cd3b2d4d08f83b5c11b003c4e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-10 03:36:42 +0300

    feat: debug mode

--- web/src/scenes/controller.ts
+++ web/src/scenes/controller.ts
@@ -2,22 +2,31 @@ import CircleButton from "../gameobjects/circle-button"
 import SquareButton from "../gameobjects/square-button"
 
 export default class Controller extends Phaser.Scene {
-  private joystick!: any
-  private dpad!: Phaser.GameObjects.Container
+  showDebugText: boolean = true
 
-  private menu_btn!: CircleButton
-  private powr_btn!: SquareButton
-  private led!: Phaser.GameObjects.Arc
+  joystick = { x: 0, y: 0 }
+  dpad = { up: false, down: false, left: false, right: false }
+  menu = false
+  power = false
+
+  private debugText!: Phaser.GameObjects.Text
+
+  private joystick_input!: any
+  private dpad_input!: Phaser.GameObjects.Container
+
+  private menu_button!: CircleButton
+  private power_button!: SquareButton
+  private led_indicator!: Phaser.GameObjects.Arc
 
   private positionUI = () => {
     const vw = this.game.config.width as number / 100
     const vh = this.game.config.height as number / 100
 
-    this.joystick.setPosition(20 * vw, 50 * vh)
-    this.menu_btn.setPosition(50 * vw, 50 * vh)
-    this.powr_btn.setPosition(50 * vw, 75 * vh)
-    this.dpad.setPosition(80 * vw, 50 * vh)
-    this.led.setPosition(50 * vw, 25 * vh)
+    this.joystick_input.setPosition(20 * vw, 50 * vh)
+    this.menu_button.setPosition(50 * vw, 50 * vh)
+    this.power_button.setPosition(50 * vw, 75 * vh)
+    this.dpad_input.setPosition(80 * vw, 50 * vh)
+    this.led_indicator.setPosition(50 * vw, 25 * vh)
   }
 
   constructor() {
@@ -34,7 +43,7 @@ export default class Controller extends Phaser.Scene {
   create() {
     this.input.addPointer(10); // if you manage to need more pointers than most people have fingers, I'll be truly impressed.
 
-    this.joystick = (this
+    this.joystick_input = (this
       .plugins
       .get('rexvirtualjoystickplugin') as any)
       .add(this, {
@@ -43,32 +52,67 @@ export default class Controller extends Phaser.Scene {
         base: this.add.circle(0, 0, 125, 0x111111),
         thumb: this.add.circle(0, 0, 55, 0x1c1c1c).setStrokeStyle(10, 0x111111)
       })
-      .on('update', () => console.log(this.joystick.forceX, this.joystick.forceY))
+      .on('update', () => {
+        console.log(this.joystick_input.forceX, this.joystick_input.forceY)
+        this.joystick.x = this.joystick_input.forceX
+        this.joystick.y = this.joystick_input.forceY
+      })
 
-    this.menu_btn = new CircleButton(this, 0, 0)
-      .on('press', () => console.log('pressed menu'))
+    this.menu_button = new CircleButton(this, 0, 0)
+      .on('release', () => this.menu = false)
+      .on('press', () => {
+        console.log('pressed menu')
+        this.menu = true
+      })
 
-    this.powr_btn = new SquareButton(this, 0, 0)
+    this.power_button = new SquareButton(this, 0, 0)
       .setFaceColor(0x00ff00)
       .setStemColor(0x00bb00)
-      .on('press', () => console.log('pressed power'))
+      .on('release', () => this.power = false)
+      .on('press', () => {
+        console.log('pressed power')
+        this.power = true
+      })
 
-    this.dpad = this
+    this.dpad_input = this
       .add
       .container(0, 0, [
         this.add.circle(0, 0, 125, 0x111111),
-        new SquareButton(this,  0, -75).on('press', () => console.log('pressed dpad up')),
-        new SquareButton(this,  0,  75).on('press', () => console.log('pressed dpad down')),
-        new SquareButton(this, -75,  0).on('press', () => console.log('pressed dpad left')),
-        new SquareButton(this,  75,  0).on('press', () => console.log('pressed dpad right')),
+        new SquareButton(this, 0, -75).on('press', () => { console.log('pressed dpad up');    this.dpad.up =    true }).on('release', () => this.dpad.up = false),
+        new SquareButton(this, 0,  75).on('press', () => { console.log('pressed dpad down');  this.dpad.down =  true }).on('release', () => this.dpad.down = false),
+        new SquareButton(this, -75, 0).on('press', () => { console.log('pressed dpad left');  this.dpad.left =  true }).on('release', () => this.dpad.left = false),
+        new SquareButton(this,  75, 0).on('press', () => { console.log('pressed dpad right'); this.dpad.right = true }).on('release', () => this.dpad.right = false),
       ])
 
-    this.led = this
+    this.led_indicator = this
       .add
       .circle(0, 0, 8, 0xffffff)
       .setStrokeStyle(4, 0xbbbbbb)
 
+    if (this.showDebugText) {
+      this.debugText = this.add
+        .text(0, 0, '[debug mode]')
+    }
+
     this.scale.on('resize', this.positionUI)
     this.positionUI()
+  }
+
+  update() {
+    let s = `[debug mode]\n`
+      + `\n`
+      + `joystick x: ${this.joystick.x}\n`
+      + `joystick y: ${this.joystick.y}\n`
+      + `\n`
+      + `dpad:\n`
+      + `  up:    ${this.dpad.up}\n`
+      + `  down:  ${this.dpad.down}\n`
+      + `  left:  ${this.dpad.left}\n`
+      + `  right: ${this.dpad.right}\n`
+      + `\n`
+      + `menu: ${this.menu}\n`
+      + `power: ${this.power}\n`
+
+    this.debugText.setText(s)
   }
 }
