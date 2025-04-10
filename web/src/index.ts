3fe1402085f3a333041835f7453fb9f1d0fda8b1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-10 07:31:34 +0300

    feat: html form ux

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,30 +1,47 @@
 const hostElement: HTMLInputElement | null = document.querySelector('#host')
 const portElement: HTMLInputElement | null = document.querySelector('#port')
 
-const button: HTMLButtonElement | null = document.querySelector('#connect')
+const connect_button: HTMLButtonElement | null = document.querySelector('#connect')
+const send_button: HTMLButtonElement | null = document.querySelector('#send')
 
 var connection: WebSocket
 
-button?.addEventListener('click', () => {
+connect_button?.addEventListener('click', () => {
   const host = hostElement?.value !== '' ? hostElement?.value : 'localhost'
   const port = portElement?.value !== '' ? portElement?.value : 8765
 
   const address = `ws://${host}:${port}`
 
-  if (!connection) {
+  if (!connection || connection.readyState === connection.CLOSED) {
     console.log(`Attempting to connect to \`${address}\`...`)
     connection = new WebSocket(address)
-  }
-
-  if (connection.readyState === connection.OPEN) {
-    connection.send('RED SPY IS IN THE BASE!')
+  } else {
+    connection.close()
   }
 
   connection.onopen = () => {
     console.log('Connection established successfully')
+    if (hostElement) hostElement.disabled = true
+    if (portElement) portElement.disabled = true
+    connect_button.textContent = 'Disconnect'
+    send_button?.classList.remove('hidden')
+  }
+
+  connection.onclose = () => {
+    console.log('Closing connection...')
+    if (hostElement) hostElement.disabled = false
+    if (portElement) portElement.disabled = false
+    connect_button.textContent = 'Connect'
+    send_button?.classList.add('hidden')
   }
 
   connection.onmessage = (event) => {
     console.log('Recieved:', event.data)
+  }
+})
+
+send_button?.addEventListener('click', () => {
+  if (connection.readyState === connection.OPEN) {
+    connection.send('RED SPY IS IN THE BASE!')
   }
 })
