1d55234ed31511f8e272b2eb9dc3d4fefc39578d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-10 07:09:01 +0300

    feat: js open websocket

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,30 @@
-const count_btn = document.querySelector("button")
+const hostElement: HTMLInputElement | null = document.querySelector('#host')
+const portElement: HTMLInputElement | null = document.querySelector('#port')
 
-let count = 0
+const button: HTMLButtonElement | null = document.querySelector('#connect')
 
-count_btn?.addEventListener('click', () => {
-  count += 1
-  count_btn.textContent = count.toString()
+var connection: WebSocket
+
+button?.addEventListener('click', () => {
+  const host = hostElement?.value !== '' ? hostElement?.value : 'localhost'
+  const port = portElement?.value !== '' ? portElement?.value : 8765
+
+  const address = `ws://${host}:${port}`
+
+  if (!connection) {
+    console.log(`Attempting to connect to \`${address}\`...`)
+    connection = new WebSocket(address)
+  }
+
+  if (connection.readyState === connection.OPEN) {
+    connection.send('RED SPY IS IN THE BASE!')
+  }
+
+  connection.onopen = () => {
+    console.log('Connection established successfully')
+  }
+
+  connection.onmessage = (event) => {
+    console.log('Recieved:', event.data)
+  }
 })
