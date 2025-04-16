866663a76dddb9426c605f3d3ec0a68f66449500
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-16 04:21:43 +0300

    feat: update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,14 +5,19 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    mklittlefs.url = "github:valyntyler/mklittlefs";
+    mklittlefs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            inputs.mklittlefs.packages.x86_64-linux.default
             arduino-cli
+            esptool
             python3
           ];
         };
