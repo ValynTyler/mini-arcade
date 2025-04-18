61914df1eb943343c94bf81b1b57220170164d23
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-19 00:22:38 +0300

    feat: ws reading uptime

--- sketch/data/script.js
+++ sketch/data/script.js
@@ -1,1 +1,12 @@
-alert("RED SPY IS IN THE BASE!")
+const uptime_element = document.querySelector("#uptime")
+
+const address = "ws://192.168.4.1/ws"
+const ws = new WebSocket(address)
+
+ws.onmessage = (event) => {
+  uptime_element.textContent = `${(event.data / 1000).toFixed(2)} seconds`
+}
+
+ws.onclose = () => {
+  console.log("Closed!")
+}
