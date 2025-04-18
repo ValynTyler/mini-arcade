b96d825821f35a9c34e26cf47a9f1a0d84b4aae4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-18 23:47:02 +0300

    fix: mklittlefs package

--- flake.nix
+++ flake.nix
@@ -5,17 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
-
-    mklittlefs.url = "github:valyntyler/mklittlefs";
-    mklittlefs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
+        packages.mklittlefs = pkgs.stdenv.mkDerivation {
+          name = "mklittlefs";
+          src = fetchGit {
+            url = "https://github.com/ValynTyler/mklittlefs.git";
+            rev = "2887dead1fd78197bed706ed78afb32521b0af6c";
+            submodules = true;
+          };
+          buildInputs = with pkgs; [ gcc ];
+          buildPhase = ''
+            make dist
+          '';
+          installPhase = ''
+            mkdir -p $out/bin/
+            cp mklittlefs $out/bin/mklittlefs
+          '';
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            inputs.mklittlefs.packages.x86_64-linux.default
+            self.packages.${system}.mklittlefs
             arduino-cli
             esptool
             python3
