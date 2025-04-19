67be8726e3528ddd5dfd0e75dead11f42335d8f7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-19 23:29:11 +0300

    feat: reading input state

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,7 +1,30 @@
+#include "dpad.h"
+#include "joystick.h"
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(13, INPUT_PULLUP);
+
+  DPad::init();
+  Joystick::init();
 }
 
 void loop() {
+  bool menu = digitalRead(13);
+
+  DPad dpad = DPad::read();
+  Joystick joystick = Joystick::read();
+
+  Serial.print("controller: ");
+  Serial.print("{ menu: ");
+  Serial.print(menu);
+  Serial.print(", dpad: ");
+  Serial.print(dpad);
+  Serial.print(", joystick: ");
+  Serial.print(joystick);
+  Serial.print(" }");
+  Serial.println();
+
   delay(10);
 }
