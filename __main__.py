7fa1bdb6681262136e0138a4ffd603be75a90832
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 04:49:44 +0300

    style: __main__.py

--- __main__.py
+++ __main__.py
@@ -6,20 +6,20 @@ from websockets.asyncio.server import serve
 
 
 async def echo(websocket):
-  async for message in websocket:
-    value = struct.unpack('<I', message)[0]
-    print('Controller state:', bin(value))
-    await websocket.send(message)
+    async for message in websocket:
+        value = struct.unpack("<I", message)[0]
+        print("Controller state:", bin(value))
+        await websocket.send(message)
 
 
 async def main():
-  try:
-    port = 8000
-    print(f'Opening WS server at ws://localhost:{port}.')
-    async with serve(echo, '0.0.0.0', port):
-      await asyncio.get_running_loop().create_future()
-  except:
-    print('\nClosing WS server...')
+    try:
+        port = 8000
+        print(f"Opening WS server at ws://localhost:{port}.")
+        async with serve(echo, "0.0.0.0", port):
+            await asyncio.get_running_loop().create_future()
+    except:
+        print("\nClosing WS server...")
 
 
 asyncio.run(main())
