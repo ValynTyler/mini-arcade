20aa88b01485880f67871122cc3aab76572a7908
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 04:59:08 +0300

    feat: update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,10 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ ];
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.pygame
+          ];
         };
       }
     );
